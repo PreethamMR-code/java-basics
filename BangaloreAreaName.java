167b217de0cb37efb28fd84ea7d50de09015deaf
PreethamMR-code <preethampreetham560@gmail.com>
2025-07-08 22:09:15 +0530

    Update BangaloreAreaName.java

--- BangaloreAreaName.java
+++ BangaloreAreaName.java
@@ -1,11 +1,10 @@
 class BangaloreAreaName {
-    public static void main(String Assignment[]) {
-	
-	String[] BangaloreAreaNames = {"vijaya nagara", "basaveshwara nagar"};
-        
-	
-	for (String area : BangaloreAreaNames) {
-    System.out.println(area);
-}
-}
+
+    public static void displayAreas() {
+        String[] BangaloreAreaNames = {"vijaya nagara", "basaveshwara nagar", "malleshwaram", "indiranagar"};
+
+        for (String area : BangaloreAreaNames) {
+            System.out.println(area);
+        }
+    }
 }
