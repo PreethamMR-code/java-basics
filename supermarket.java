41d949e38e50c56412f9d90064344bd41ed59e90
PreethamMR-code <preethampreetham560@gmail.com>
2025-07-08 22:34:15 +0530

    Update supermarket.java

--- supermarket.java
+++ supermarket.java
@@ -1,11 +1,18 @@
-class supermarket
-{
-	public static void main(String args[])
-	{
-		 String[] superMarketIceCreams = {"Ruby Red Velvet", "Choco Peanut Fudge", "Nutty Choco Caramel", "Fruit Burst", "Chocolate Crush", "Fruity Mango Tango", "Cookie Shot", "Berry Crunch", "Butterscotch Bananza", "Brownie Fudge"};
-		 
-		 for (String iceCream : superMarketIceCreams) {
-    System.out.println(iceCream);
-}
-}
+class supermarket {
+
+    public static void displayIceCreams() {
+        String[] superMarketIceCreams = {
+            "Ruby Red Velvet", "Choco Peanut Fudge", "Nutty Choco Caramel",
+            "Fruit Burst", "Chocolate Crush", "Fruity Mango Tango",
+            "Cookie Shot", "Berry Crunch", "Butterscotch Bananza", "Brownie Fudge"
+        };
+
+        for (String iceCream : superMarketIceCreams) {
+            System.out.println(iceCream);
+        }
+    }
+
+    public static void main(String[] args) {
+        displayIceCreams();
+    }
 }
