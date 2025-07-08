13e2757bb1b3754dac0a5fc346402d4e8685413d
PreethamMR-code <preethampreetham560@gmail.com>
2025-07-08 22:35:43 +0530

    Update tour.java

--- tour.java
+++ tour.java
@@ -1,11 +1,18 @@
-class tour
-{
-	public static void main(String args[])
-	{
-	String[] touristPlaces = {"iskon temple", "mysuru palace", "lalbagh", "nandi hills", "jog falls", "hampi", "coorg", "madikeri", "gokarna", "kudremuka", "dandeli", "murudeshwra", "badami caves", "bhadra tiger reservoir", "srirangapatna"};
-        
-		for (String place : touristPlaces) {
-    System.out.println(place);
-}
-}
+class tour {
+
+    public static void displayTouristPlaces() {
+        String[] touristPlaces = {
+            "iskon temple", "mysuru palace", "lalbagh", "nandi hills", "jog falls",
+            "hampi", "coorg", "madikeri", "gokarna", "kudremuka",
+            "dandeli", "murudeshwra", "badami caves", "bhadra tiger reservoir", "srirangapatna"
+        };
+
+        for (String place : touristPlaces) {
+            System.out.println(place);
+        }
+    }
+
+    public static void main(String[] args) {
+        displayTouristPlaces();
+    }
 }
