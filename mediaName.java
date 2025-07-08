b584a742ca2791222d0a819d522210de7e2a61fe
PreethamMR-code <preethampreetham560@gmail.com>
2025-07-08 22:31:20 +0530

    Update mediaName.java

--- mediaName.java
+++ mediaName.java
@@ -1,10 +1,17 @@
 class mediaName {
-    public static void main(String Assignment[]) {
-	
-	String[] mediaNames = {"Facebook", "Instagram", "Twitter", "LinkedIn", "TikTok", "Snapchat", "Pinterest", "YouTube", "Reddit", "Threads"};
 
-for (String media : mediaNames) {
-    System.out.println(media);
-}
-}
+    public static void displayMediaNames() {
+        String[] mediaNames = {
+            "Facebook", "Instagram", "Twitter", "LinkedIn", "TikTok",
+            "Snapchat", "Pinterest", "YouTube", "Reddit", "Threads"
+        };
+
+        for (String media : mediaNames) {
+            System.out.println(media);
+        }
+    }
+
+    public static void main(String[] args) {
+        displayMediaNames();
+    }
 }
