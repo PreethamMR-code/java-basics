9587d1923ec8bbfe47d77a746f9624d9e7aa6aaf
PreethamMR-code <preethampreetham560@gmail.com>
2025-07-08 22:32:12 +0530

    Update playstation.java

--- playstation.java
+++ playstation.java
@@ -1,12 +1,18 @@
-class playstation
-{
-	public static void main(String args[])
-	{
-	 String[] playstationGames = {"The Last of Us Part II", "God of War Ragnarök", "Marvel's Spider-Man 2", "Horizon Forbidden West", "Ghost of Tsushima", "Final Fantasy VII Remake", "Elden Ring", "Resident Evil 4 Remake", "Ratchet & Clank: Rift Apart", "Stray"};
-    
-	for (String game : playstationGames) {
-    System.out.println(game);
-}
+class playstation {
 
-}
+    public static void displayPlaystationGames() {
+        String[] playstationGames = {
+            "The Last of Us Part II", "God of War Ragnarök", "Marvel's Spider-Man 2",
+            "Horizon Forbidden West", "Ghost of Tsushima", "Final Fantasy VII Remake",
+            "Elden Ring", "Resident Evil 4 Remake", "Ratchet & Clank: Rift Apart", "Stray"
+        };
+
+        for (String game : playstationGames) {
+            System.out.println(game);
+        }
+    }
+
+    public static void main(String[] args) {
+        displayPlaystationGames();
+    }
 }
