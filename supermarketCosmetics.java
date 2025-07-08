75c9c4fbb395aabe69ee0955b7bbe389dfddc7e0
PreethamMR-code <preethampreetham560@gmail.com>
2025-07-08 22:34:41 +0530

    Update supermarketCosmetics.java

--- supermarketCosmetics.java
+++ supermarketCosmetics.java
@@ -1,12 +1,17 @@
-class supermarketCosmetics
-{
-	public static void main(String args[])
-	{
-	String[] superMarketCosmetics = {"eyeliner", "concealer", "foundation", "mascara", "lipstick", "sunscreen", "vitamin c serum", "moisturizer", "nail polish", "face wash"};
-       
-	
-	for (String cosmetic : superMarketCosmetics) {
-    System.out.println(cosmetic);
-}
-}
+class supermarketCosmetics {
+
+    public static void displayCosmetics() {
+        String[] superMarketCosmetics = {
+            "eyeliner", "concealer", "foundation", "mascara", "lipstick",
+            "sunscreen", "vitamin c serum", "moisturizer", "nail polish", "face wash"
+        };
+
+        for (String cosmetic : superMarketCosmetics) {
+            System.out.println(cosmetic);
+        }
+    }
+
+    public static void main(String[] args) {
+        displayCosmetics();
+    }
 }
