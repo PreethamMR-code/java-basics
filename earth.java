1c69a01d6e021b030427103d1c1c7c781732c565
PreethamMR-code <preethampreetham560@gmail.com>
2025-07-08 22:28:37 +0530

    Update earth.java

--- earth.java
+++ earth.java
@@ -1,12 +1,17 @@
-class earth
-{
-	public static void main(String args[])
-	{
-	String[] earthContinenets = {"asia", "australia", "south america", "north america", "antarctica", "africa", "europe"};
-      
-	  
-	  for (String continent : earthContinenets) {
-    System.out.println(continent);
-}
-}
+class earth {
+
+    public static void displayContinents() {
+        String[] earthContinents = {
+            "asia", "australia", "south america", "north america",
+            "antarctica", "africa", "europe"
+        };
+
+        for (String continent : earthContinents) {
+            System.out.println(continent);
+        }
+    }
+
+    public static void main(String[] args) {
+        displayContinents();
+    }
 }
