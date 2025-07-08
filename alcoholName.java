6e5db602e70f2fe628b6d0d80504e5d29be3ca21
PreethamMR-code <preethampreetham560@gmail.com>
2025-07-08 22:17:40 +0530

    Update alcoholName.java

--- alcoholName.java
+++ alcoholName.java
@@ -1,11 +1,14 @@
-class alcoholName {
-    public static void main(String Assignment[]) {
-	
-	 String[] alcoholNames = {"Vodka", "Whiskey", "Rum", "Gin", "Tequila", "Brandy", "Red Wine", "White Wine", "Beer", "Champagne", "Cognac", "Sake"};
-    
-	
-	for (String alcohol : alcoholNames) {
-    System.out.println(alcohol);
-}
-}
+class AlcoholName {
+
+    public static void printAlcoholNames() {
+        String[] alcoholNames = {
+            "Vodka", "Whiskey", "Rum", "Gin", "Tequila",
+            "Brandy", "Red Wine", "White Wine", "Beer",
+            "Champagne", "Cognac", "Sake"
+        };
+
+        for (String alcohol : alcoholNames) {
+            System.out.println(alcohol);
+        }
+    }
 }
