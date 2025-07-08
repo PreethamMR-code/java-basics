b6c3c0e03e24abbb31dbb2750764ead764d2f104
PreethamMR-code <preethampreetham560@gmail.com>
2025-07-08 22:35:11 +0530

    Update supermarketGroceries.java

--- supermarketGroceries.java
+++ supermarketGroceries.java
@@ -1,12 +1,17 @@
-class supermarketGroceries
-{
-	public static void main(String args[])
-	{
-		
-	 String[] superMarketGroceries = {"Coffee", "teabags", "milk", "Rice", "Oats", "powdered sugar", "Shampoo", "toothpaste", "Bread", "cookies"};
-	
-	for (String grocery : superMarketGroceries) {
-    System.out.println(grocery);
-}
-}
+class supermarketGroceries {
+
+    public static void displayGroceries() {
+        String[] superMarketGroceries = {
+            "Coffee", "teabags", "milk", "Rice", "Oats",
+            "powdered sugar", "Shampoo", "toothpaste", "Bread", "cookies"
+        };
+
+        for (String grocery : superMarketGroceries) {
+            System.out.println(grocery);
+        }
+    }
+
+    public static void main(String[] args) {
+        displayGroceries();
+    }
 }
