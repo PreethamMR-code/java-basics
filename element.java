46f3aea7f89d1451f6fe63587b2bc6bd7b3b770b
PreethamMR-code <preethampreetham560@gmail.com>
2025-07-08 22:29:07 +0530

    Update element.java

--- element.java
+++ element.java
@@ -1,11 +1,18 @@
 class element {
-    public static void main(String Assignment[]) {
-	
-	        String[] elements = {"Hydrogen", "Helium", "Lithium", "Beryllium", "Boron", "Carbon", "Nitrogen", "Oxygen", "Flurine", "Neon", "Sodium", "Magnesium"};
-        
-		
-		for (String element : elements) {
-    System.out.println(element);
-}
-}
+
+    public static void displayElements() {
+        String[] elements = {
+            "Hydrogen", "Helium", "Lithium", "Beryllium", "Boron",
+            "Carbon", "Nitrogen", "Oxygen", "Flurine", "Neon",
+            "Sodium", "Magnesium"
+        };
+
+        for (String element : elements) {
+            System.out.println(element);
+        }
+    }
+
+    public static void main(String[] args) {
+        displayElements();
+    }
 }
