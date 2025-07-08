f1ce946b5d7af5ba2d2b4735498d9848a75d8886
PreethamMR-code <preethampreetham560@gmail.com>
2025-07-08 22:26:31 +0530

    Update brand.java

--- brand.java
+++ brand.java
@@ -1,12 +1,19 @@
 class brand {
-    public static void main(String Assignment[]) {
-	
-	String[] brands = {"Pavitra Silk Sarees", "Pavitra Cotton Kurtis", "Pavitra Bridal Lehengas", "Pavitra Festive Suits", "Pavitra Casual Wear", "Pavitra Kids Collection", "Pavitra Mens Ethnic Wear", "Designer Collection (Various Designers)", "Traditional Handlooms", "Contemporary Fusion Wear"};
-       
-	
-	
-	for (String brand : brands) {
-    System.out.println(brand);
-}
-}
+
+    public static void displayBrands() {
+        String[] brands = {
+            "Pavitra Silk Sarees", "Pavitra Cotton Kurtis", "Pavitra Bridal Lehengas",
+            "Pavitra Festive Suits", "Pavitra Casual Wear", "Pavitra Kids Collection",
+            "Pavitra Mens Ethnic Wear", "Designer Collection (Various Designers)",
+            "Traditional Handlooms", "Contemporary Fusion Wear"
+        };
+
+        for (String brand : brands) {
+            System.out.println(brand);
+        }
+    }
+
+    public static void main(String[] args) {
+        displayBrands();
+    }
 }
